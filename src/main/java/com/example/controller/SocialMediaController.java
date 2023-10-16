a109d28566a42dd4f4af20a20f45d9c43a4f9107
Chalfton <zachjdevore@hotmail.com>
2023-10-16 15:06:55 -0400

    Pushing to fix weird bug

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,37 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    private AccountService accountService;
+    private MessageService messageService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    // Our API should be able to process new User registrations
+    @PostMapping("/register")
+    public void registerNewUser(@RequestBody Account account) {
+        accountService.addNewUser(account);
+    }
+
+    // Our API should be able to process User logins
+
+    // Our API should be able to process the creation of new messages
+
+    // Our API should be able to retrieve all messages
+
+    // Our API should be able to retrieve a message by its ID
+
+    // Our API should be able to delete a message identified by a message ID
+
+    // Our API should be able to update a message text identified by a message ID
+
+    // Our API should be able to retrieve all messages written by a particular user
+
+    
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,23 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    // Add a new account
+    public Account addNewUser(Account account) {
+        return accountRepository.save(account);
+    }
 }
